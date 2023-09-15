4a8777aeb117f9d590c568433c0daf7d0c379eb7
chandrushan24 <chandru.sankar2411@gmail.com>
2023-09-15 18:03:16 +0530

    test

--- application/controllers/Crud.php
+++ application/controllers/Crud.php
@@ -14,6 +14,7 @@ class Crud extends CI_Controller
          $this->load->helper('url');
         }
         
+        
         public function savedata()
         {
         $this->load->view('insert');
